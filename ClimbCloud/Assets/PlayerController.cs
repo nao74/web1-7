8c87ddc9c6ae7935d6e0140bd38ec452ec612dd4
Unknown <naoyat22@gmail.com>
2018-06-18 17:09:17 +0900

    猫ジャンプ　シーン移行＆バグなくし

--- ClimbCloud/Assets/PlayerController.cs
+++ ClimbCloud/Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
@@ -17,32 +18,43 @@ public class PlayerController : MonoBehaviour {
         this.animator = GetComponent<Animator>();
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-        if(Input.GetKeyDown(KeyCode.Space))
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
         int key = 0;
-        if (Input.GetKey(KeyCode.RightArrow)) key= 1;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
         if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
 
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
 
-        if(speedx<this.maxWalkSpeed)
+        if (speedx < this.maxWalkSpeed)
         {
             this.rigid2D.AddForce(transform.right * key * this.walkForce);
         }
 
-        if(key!=0)
+        if (key != 0)
         {
             transform.localScale = new Vector3(key, 1, 1);
         }
 
         this.animator.speed = speedx / 2.0f;
 
-	}
+        if(transform.position.y<-10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
+    }
 }
